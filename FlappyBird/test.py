74547a705a5d7363cd0c913e39ffc5232337ed33
Marjick7 <76257717+Marjick7@users.noreply.github.com>
2021-11-30 12:28:40 +0530

    Add files via upload

---
+++ FlappyBird/test.py
@@ -0,0 +1,10 @@
+class Person:
+
+   def __init__(self, name, age, power):
+      self.name = name
+      self.age = age
+      self.power = power
+
+person1 = Person("marjick", "20", "physicalcontact")
+
+print(person1)
